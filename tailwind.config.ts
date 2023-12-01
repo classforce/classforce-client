ea55521b6113569c2eb37820f99b7cf505dd819a
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-12-01 12:26:32 +0100

    setup tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,23 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#ffbd59',
+        dark: '#161006',
+        white: '#ffffff',
+        green: '#32d76a',
+        red: '#ed4337',
+      },
+      fontSize: {
+        display: ['67.34px', { lineHeight: '100%', fontWeight: 'bold' }],
+        'heading-l': ['39.9px', { lineHeight: '100%', fontWeight: 'medium' }],
+        'heading-m': ['28.43px', { lineHeight: '100%', fontWeight: 'medium' }],
+        'heading-s': ['21.33px', { lineHeight: '100%', fontWeight: 'medium' }],
+        body: ['16px', { lineHeight: '150%', fontWeight: 'regular' }],
+        small: ['14px', { lineHeight: '150%', fontWeight: 'regular' }],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
